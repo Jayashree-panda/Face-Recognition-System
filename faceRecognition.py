6051b742da8d93cc35523591987a06eb4e752994
Jayashree-panda <jayashreepanda12345@gmail.com>
2020-05-14 10:24:33 +0530

    plotting images and showig predictions

--- faceRecognition.py
+++ faceRecognition.py
@@ -134,27 +134,62 @@ print(newTestX.shape)
 # save arrays to one file in compressed format
 savez_compressed('5-celebrity-faces-embeddings.npz', newTrainX, trainy, newTestX, testy)
 
+# load faces
+data = load('5-celebrity-faces-dataset.npz')
+testX_faces = data['arr_2']
+
 # load dataset
 data = load('5-celebrity-faces-embeddings.npz')
 trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
 print('Dataset: train=%d, test=%d' % (trainX.shape[0], testX.shape[0]))
+
 # normalize input vectors
 in_encoder = Normalizer(norm='l2')
 trainX = in_encoder.transform(trainX)
 testX = in_encoder.transform(testX)
+
 # label encode targets
 out_encoder = LabelEncoder()
 out_encoder.fit(trainy)
 trainy = out_encoder.transform(trainy)
 testy = out_encoder.transform(testy)
+
 # fit model
 model = SVC(kernel='linear', probability=True)
 model.fit(trainX, trainy)
+
 # predict
 yhat_train = model.predict(trainX)
 yhat_test = model.predict(testX)
+
 # score
 score_train = accuracy_score(trainy, yhat_train)
 score_test = accuracy_score(testy, yhat_test)
+
 # summarize
 print('Accuracy: train=%.3f, test=%.3f' % (score_train*100, score_test*100))
+
+# test model on a random example from the test dataset
+selection = choice([i for i in range(testX.shape[0])])
+random_face_pixels = testX_faces[selection]
+random_face_emb = testX[selection]
+random_face_class = testy[selection]
+random_face_name = out_encoder.inverse_transform([random_face_class])
+
+# prediction for the face
+samples = expand_dims(random_face_emb, axis=0)
+yhat_class = model.predict(samples)
+yhat_prob = model.predict_proba(samples)
+
+# get name
+class_index = yhat_class[0]
+class_probability = yhat_prob[0,class_index] * 100
+predict_names = out_encoder.inverse_transform(yhat_class)
+print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
+print('Expected: %s' % random_face_name[0])
+
+# plot for fun
+pyplot.imshow(random_face_pixels)
+title = '%s (%.3f)' % (predict_names[0], class_probability)
+pyplot.title(title)
+pyplot.show()
