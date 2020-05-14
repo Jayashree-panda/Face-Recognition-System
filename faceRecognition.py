eb5454fe0c22b58852c86d66ba726e6825be4bbd
Jayashree-panda <jayashreepanda12345@gmail.com>
2020-05-14 09:07:47 +0530

    detecting faces

--- faceRecognition.py
+++ faceRecognition.py
@@ -36,6 +36,48 @@ def extract_face(filename, required_size=(160, 160)):
 	image = image.resize(required_size)
 	face_array = asarray(image)
 	return face_array
- 
-# load the photo and extract the face
-pixels = extract_face('test_image.jpeg')
+
+
+# load images and extract faces for all images in a directory
+def load_faces(directory):
+	faces = list()
+	# enumerate files
+	for filename in listdir(directory):
+		# path
+		path = directory + filename
+		# get face
+		face = extract_face(path)
+		# store
+		faces.append(face)
+	return faces
+
+	
+# load a dataset that contains one subdir for each class that in turn contains images
+def load_dataset(directory):
+	X, y = list(), list()
+	# enumerate folders, on per class
+	for subdir in listdir(directory):
+		# path
+		path = directory + subdir + '/'
+		# skip any files that might be in the dir
+		if not isdir(path):
+			continue
+		# load all faces in the subdirectory
+		faces = load_faces(path)
+		# create labels
+		labels = [subdir for _ in range(len(faces))]
+		# summarize progress
+		print('>loaded %d examples for class: %s' % (len(faces), subdir))
+		# store
+		X.extend(faces)
+		y.extend(labels)
+	return asarray(X), asarray(y)
+
+# load train dataset
+trainX, trainy = load_dataset('5-celebrity-faces-dataset/train/')
+print(trainX.shape, trainy.shape)
+# load test dataset
+testX, testy = load_dataset('5-celebrity-faces-dataset/val/')
+print(testX.shape, testy.shape)
+# save arrays to one file in compressed format
+savez_compressed('5-celebrity-faces-dataset.npz', trainX, trainy, testX, testy)
